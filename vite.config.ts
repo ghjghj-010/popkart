e8fbd908411dcab1cdedc97183d3fb55c706dc44
ghjghj-010 <625094836@qq.com>
2025-10-04 23:20:58 +0800

    feat: deploy yaml

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 如果您的仓库名称不是"aiKart"，请替换为您的实际仓库名称
+  base: '/popKart/'
 })
